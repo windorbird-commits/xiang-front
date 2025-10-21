eb044b54304fc3f19c9060739edc6de6018caa43
Tom <windorbird@gmail.com>
2025-10-21 21:04:52 +0800

    香方构图

---
+++ src/components/XiangFangItem/XiangFangItem.vue
@@ -0,0 +1,43 @@
+<template>
+    <view class="container">
+        <view class="top">
+        </view>
+        <view class="mid"></view>
+        <view class="bottom">
+
+        </view>
+
+    </view>
+</template>
+
+
+<script setup lang="ts">
+</script>
+
+<style scoped>
+    .container {
+        width: 700rpx;
+        height: 300rpx;
+        border: 1px solid red;
+        border-radius: 20rpx;
+    }
+
+    .top {
+        width: 100%;
+        height: 30%;
+        border: 2px solid blue;
+    }
+
+    .mid {
+        width: 100%;
+        height: 40%;
+        border: 2px solid blue;
+    }
+
+    .bottom {
+        width: 100%;
+        height: 30%;
+        border: 2px solid blue;
+
+    }
+</style>
--- src/pages/xiangFangList/XiangFangList.vue
+++ src/pages/xiangFangList/XiangFangList.vue
@@ -1,8 +1,11 @@
 <template>
-    <view>
-        你好
+    <view class="container">
 
         <text>列表</text>
+        <view v-for="i in 10">
+            <XiangFangItem class="xiangFangItem"></XiangFangItem>
+        </view>
+
     </view>
 </template>
 
@@ -10,6 +13,19 @@
 
 </script>
 
-<style>
+<style scoped>
+    .container {
+        box-sizing: border-box;
+    }
 
+    .xiangFangItem {
+        margin: 20rpx;
+        display: -webkit-flex;
+        /* H5兼容性前缀 */
+        justify-content: center;
+        align-items: center;
+        box-sizing: border-box;
+        /* 统一盒模型 */
+
+    }
 </style>
