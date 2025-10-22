bfab87a195b22e79008276b119145fcf93ef59a3
Tom <windorbird@gmail.com>
2025-10-22 11:33:04 +0800

    格式化

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,18 @@
 <script setup lang="ts">
-import { onLaunch, onShow, onHide } from "@dcloudio/uni-app";
-onLaunch(() => {
-  console.log("App Launch");
-});
-onShow(() => {
-  console.log("App Show");
-});
-onHide(() => {
-  console.log("App Hide");
-});
+    import { onLaunch, onShow, onHide } from "@dcloudio/uni-app";
+    onLaunch(() => {
+        console.log("App Launch");
+    });
+    onShow(() => {
+        console.log("App Show");
+    });
+    onHide(() => {
+        console.log("App Hide");
+    });
 </script>
 <style>
-/* 统一设置所有平台的页面背景色 */
-page{
-    background-color: #f7f1e3;
-}
-
-
+    /* 统一设置所有平台的页面背景色 */
+    page {
+        background-color: #f7f1e3;
+    }
 </style>
