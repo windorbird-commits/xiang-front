9b27a6662f79e173833e45e651fb9e76d84c34da
Tom <windorbird@gmail.com>
2025-10-22 14:22:12 +0800

    删除重复

--- src/pages/xiangFangList/XiangFangList.vue
+++ src/pages/xiangFangList/XiangFangList.vue
@@ -12,10 +12,6 @@
 </script>
 
 <style lang="scss" scoped>
-    .container {
-        box-sizing: border-box;
-    }
-
     .xiangFangItem {
         width: 690rpx;
         margin: 50rpx auto;
@@ -23,8 +19,5 @@
         /* H5兼容性前缀 */
         justify-content: center;
         align-items: center;
-        box-sizing: border-box;
-        /* 统一盒模型 */
-
     }
 </style>
