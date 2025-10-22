111d2aa2621835e959d78e6003aa731fa46a5c38
Tom <windorbird@gmail.com>
2025-10-22 15:45:50 +0800

    暂存

---
+++ src/components/tool-component/tool-component.vue
@@ -0,0 +1,18 @@
+<template>
+    <view class="container">
+        hello
+
+    </view>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style lang="scss">
+    .container {
+        width: 300rpx;
+        height: 300rpx;
+        border: 1px solid red;
+    }
+</style>
