92571f682a94197719d9a655548618d7a6e6e6fe
Tom <windorbird@gmail.com>
2025-10-30 22:14:39 +0800

    引入 pinia

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,14 @@
 import { createSSRApp } from "vue";
+import { createPinia } from "pinia";
 import App from "./App.vue";
+
+
 export function createApp() {
+    const pinia = createPinia()
     const app = createSSRApp(App);
+
+    app.use(pinia)
+
     return {
         app,
     };
